77893435f9d30121a63f6ebec8ba24f2079c10d8
Malicia <a.b@c.com>
2025-07-03 11:28:06 -0400

    Add navigation bar on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import './App.css';
 
-function App() {
+interface Page {
+	name: string;
+}
+
+interface Props {
+	pages: Page[];
+}
+
+function App({ pages = [] }: Props) {
 	return (
 		<main>
 			<h1>Accueil</h1>
+			<nav>
+				<ul>
+					{pages.map((page) => {
+						return (
+							<li>
+								<a>{page.name}</a>
+							</li>
+						);
+					})}
+				</ul>
+			</nav>
 		</main>
 	);
 }
