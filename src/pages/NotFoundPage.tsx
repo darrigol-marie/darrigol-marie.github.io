d5e335b6098424581fdffe4c73d61e678991a857
Malicia <a.b@c.com>
2025-08-27 11:14:55 -0400

    Add link to home page on Not Found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,8 +1,11 @@
+import { NavLink } from 'react-router-dom';
+
 function NotFoundPage() {
 	return (
 		<>
 			<h1>Erreur 404</h1>
 			<article>La page que vous cherchez n'existe pas ou a été déplacée.</article>
+			<NavLink to="/">Retour à l'accueil</NavLink>
 		</>
 	);
 }
