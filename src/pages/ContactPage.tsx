b48f201fe8e9b7e373674d51f6b0ff8b55e1123a
Malicia <a.b@c.com>
2026-01-30 15:33:41 -0500

    Update Contact page text

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,13 +1,20 @@
 function ContactPage() {
 	return (
 		<>
-			<a href="mailto:darrigol.marie@gmail.com">darrigol.marie@gmail.com</a>
-			<a
-				href="https://www.linkedin.com/in/marie-darrigol-a7844251/"
-				target="_blank"
-			>
-				Mon profil LinkedIn
-			</a>
+			<p>Vous souhaitez me contacter ?</p>
+			<p>
+				Vous pouvez m’écrire à{' '}
+				<a href="mailto:darrigol.marie@gmail.com">darrigol.marie@gmail.com</a> ou
+				venir discuter sur{' '}
+				<a
+					href="https://www.linkedin.com/in/marie-darrigol-a7844251/"
+					target="_blank"
+				>
+					LinkedIn
+				</a>
+				.
+			</p>
+			<p>Au plaisir de vous lire !</p>
 		</>
 	);
 }
