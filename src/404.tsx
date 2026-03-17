36f79c0862bfb43f63dfc676b1843e3dfa54cdaa
Marie Darrigol <darrigol.marie@gmail.com>
2026-03-17 13:48:43 -0400

    Fix error 404 page rendering

--- src/404.tsx
+++ src/404.tsx
@@ -1,19 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 import './index.scss';
 
-import NotFoundPage from './pages/NotFoundPage';
-
-const router = createHashRouter([
-	{
-		element: <NotFoundPage />,
-	},
-]);
+import Header from './components/Header';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<Header title="Erreur 404" />
+		<div id="page">
+			<article>La page que vous cherchez n'existe pas ou a été déplacée.</article>
+			<a href="/">Retour à l'accueil</a>
+		</div>
 	</StrictMode>,
 );
