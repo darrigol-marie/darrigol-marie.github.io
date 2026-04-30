b7676368153e604b55b6e9616d95e458734552fa
Marie Darrigol <darrigol.marie@gmail.com>
2026-04-30 16:56:55 -0400

    Add navigation bar on App component

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -4,14 +4,22 @@ import App from '../src/App';
 
 describe('App', () => {
 	it('should display the main content', () => {
-		render(<App />);
+		render(<App pages={[]} />);
 
 		expect(screen.getByRole('main')).toBeInTheDocument();
 	});
 
 	it('should display a title saying "Accueil"', () => {
-		render(<App />);
+		render(<App pages={[]} />);
 
 		expect(screen.getByRole('heading')).toHaveTextContent(/accueil/i);
+	});
+
+	it('should display a navigation bar with a link to each page', () => {
+		render(<App pages={[{ name: 'Home' }]} />);
+
+		const navigationBar = screen.getByRole('navigation');
+
+		expect(navigationBar.getElementsByTagName('a')).toHaveLength(1);
 	});
 });
