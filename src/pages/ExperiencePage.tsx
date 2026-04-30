8b6d25a0d11b4eab21ecc5eedf402cc681169614
Marie Darrigol <darrigol.marie@gmail.com>
2026-04-30 16:57:07 -0400

    Move loading screen from Experience and Projects pages to Posts component

--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -19,23 +19,18 @@ function ExperiencePage() {
 	});
 
 	return (
-		<>
-			{isLoading ? (
-				<p>Chargement...</p>
-			) : (
-				<PostsList
-					posts={data.map((experience: Experience) => {
-						return {
-							id: experience.id,
-							title: experience.position,
-							subtitle: experience.company,
-							date: experience.date,
-							text: experience.description,
-						};
-					})}
-				/>
-			)}
-		</>
+		<PostsList
+			isLoading={isLoading}
+			posts={data.map((experience: Experience) => {
+				return {
+					id: experience.id,
+					title: experience.position,
+					subtitle: experience.company,
+					date: experience.date,
+					text: experience.description,
+				};
+			})}
+		/>
 	);
 }
 
