1727e8c1df74994ab6441a40a91dcbf9d1733f3e
Marie Darrigol <darrigol.marie@gmail.com>
2026-04-30 16:57:04 -0400

    Remove unused title on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
-import { appRoutes, type AppRoute } from './App.routes';
+import { appRoutes } from './App.routes';
 import './App.scss';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Navigation from './components/Navigation';
 
-function getCurrentRoute(): AppRoute | null {
-	const currentLocation = useLocation();
-
-	// TODO: throw an error instead of returning null (not finding the current route means we are on an invalid route)
-	return (
-		appRoutes.find((appRoute) => appRoute.path === currentLocation.pathname) ||
-		null
-	);
-}
-
 function App() {
-	const currentRoute = getCurrentRoute();
-
 	return (
 		<>
 			<header id="title">
-				<Header title={currentRoute ? currentRoute.name : 'Title'} />
+				<Header />
 			</header>
 			<div id="page" className="hidden-overflow">
 				<div id="content" className="hidden-overflow">
