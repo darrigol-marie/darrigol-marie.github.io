3f560c7499159ef22ae800e9a0aa2a3c257fb5cd
Marie Darrigol <darrigol.marie@gmail.com>
2026-04-30 16:57:06 -0400

    Integrate React Query for Experience page + integrate MSW to mock data fetching for tests

---
+++ src/mocks/data.ts
@@ -0,0 +1,11 @@
+import type { Experience } from "../pages/ExperiencePage";
+
+export const mockupExperiences: Experience[] = [
+    {
+        id: 'experience-test',
+        date: '2017 - 2022',
+        position: 'Développeuse front-end',
+        company: 'Digital Shape Technologies',
+        description: 'Description pour ce poste',
+    },
+];
