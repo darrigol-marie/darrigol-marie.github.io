8374813e59c1061fff92b2777f7f402a78fc6cca
Marie Darrigol <darrigol.marie@gmail.com>
2026-04-30 16:57:05 -0400

    Add link to home page on Not Found page

--- tests/pages/NotFoundPage.test.tsx
+++ tests/pages/NotFoundPage.test.tsx
@@ -1,18 +1,37 @@
 import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+
 import NotFoundPage from '../../src/pages/NotFoundPage';
+import { BrowserRouter } from 'react-router-dom';
+import { navigateTo } from '../utils/router.helper';
 
 describe('NotFoundPage', () => {
+	function renderComponent() {
+		render(<NotFoundPage />, { wrapper: BrowserRouter });
+	}
+
 	it('should display a title stating "404 error"', () => {
-		render(<NotFoundPage />);
+		renderComponent();
 
 		expect(screen.getByRole('heading')).toHaveTextContent('Erreur 404');
 	});
 
 	it('should display a text stating what happened', () => {
-		render(<NotFoundPage />);
+		renderComponent();
 
 		expect(screen.getByRole('article')).toHaveTextContent(
 			/la page.+(n'existe pas|a été déplacée)/i
 		);
+	});
+
+	it('should display a link to the home page', async () => {
+		const user = userEvent.setup();
+		navigateTo('/bad-route');
+
+		const returnLink = screen.getByRole('link');
+		expect(returnLink).toHaveTextContent(/retour.+accueil/i);
+
+		await user.click(returnLink);
+		expect(screen.getByText(/à propos/i));
 	});
 });
