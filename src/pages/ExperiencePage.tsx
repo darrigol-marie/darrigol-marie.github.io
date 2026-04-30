049e8cf80da95c173901c2db6225bb0cef5251bc
Marie Darrigol <darrigol.marie@gmail.com>
2026-04-30 16:56:59 -0400

    Add date, job title, company name and experience description on Experience page

--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -1,5 +1,14 @@
 function ExperiencePage() {
-	return <article>Toto</article>;
+	return (
+		<article>
+			<header>
+				<time dateTime="2017 - 2022">2017 - 2022</time>
+				<h2>Développeuse web</h2>
+				<h3>Digital Shape Technologies</h3>
+			</header>
+			<p>Une description de mon travail dans cette entreprise</p>
+		</article>
+	);
 }
 
 export default ExperiencePage;
--- tests/pages/ExperiencePage.test.tsx
+++ tests/pages/ExperiencePage.test.tsx
@@ -3,9 +3,55 @@ import { render, screen } from '@testing-library/react';
 import ExperiencePage from '../../src/pages/ExperiencePage';
 
 describe('ExperiencePage', () => {
-	it('should display a list of my experiences', () => {
+	const jobTitlePattern = 'développeuse|stagiaire';
+
+	function getHeadingSearchPattern(forJobTitle: boolean): RegExp {
+		return forJobTitle
+			? RegExp(`${jobTitlePattern}`, 'i')
+			: RegExp(`^((?!${jobTitlePattern}).)*$`, 'i');
+	}
+
+	function renderComponent(): { experiences: HTMLElement[] } {
 		render(<ExperiencePage />);
 
-		expect(screen.getAllByRole('article')).not.toHaveLength(0);
+		return { experiences: screen.getAllByRole('article') };
+	}
+
+	it('should display a list of my experiences', () => {
+		const component = renderComponent();
+
+		expect(component.experiences).not.toHaveLength(0);
+	});
+
+	it('should display the date for each experience', () => {
+		const component = renderComponent();
+
+		expect(screen.getAllByRole('time')).toHaveLength(
+			component.experiences.length
+		);
+	});
+
+	it('should display the job title for each experience', () => {
+		const component = renderComponent();
+
+		expect(
+			screen.getAllByRole('heading', { name: getHeadingSearchPattern(true) })
+		).toHaveLength(component.experiences.length);
+	});
+
+	it('should display the company name for each experience', () => {
+		const component = renderComponent();
+
+		expect(
+			screen.getAllByRole('heading', { name: getHeadingSearchPattern(false) })
+		).toHaveLength(component.experiences.length);
+	});
+
+	it('should display the description of each experience', () => {
+		const component = renderComponent();
+
+		expect(screen.getAllByRole('paragraph')).toHaveLength(
+			component.experiences.length
+		);
 	});
 });
